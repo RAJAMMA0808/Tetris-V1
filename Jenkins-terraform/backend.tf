fcb4f46d978ef41c12bf7105d43e075e80066505
RAJAMMA0808 <144823599+RAJAMMA0808@users.noreply.github.com>
2023-11-07 09:26:38 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,7 +1,9 @@
 terraform {
-  backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
+ backend "s3" {
+    bucket = "great-name-terraform-state-2"
+    key    = "global/s3/terraform.tfstate"
     region = "us-east-1"
+    access_key = "AKIAWGGXJJWKPUDUVPD2"
+    secret_key = "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"
   }
 }
