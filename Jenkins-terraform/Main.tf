93fa28f98e6a78d1b170c595682e85bf8f09d4f5
RAJAMMA0808 <144823599+RAJAMMA0808@users.noreply.github.com>
2023-11-07 20:15:07 +0530

    Update Main.tf

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -68,7 +68,7 @@ resource "aws_instance" "web" {
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
 
   tags = {
-    Name = "Jenkins-sonar"
+    Name = "Jenkins-ARGO"
   }
 
   root_block_device {
